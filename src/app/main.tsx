980cbbcc8f7efb738637a6c90b4b88d4bb8e6a04
lavaboat <elieatamech@hotmail.com>
2026-01-10 20:31:25 +0200

    cleaned startup code

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import "../design/styling/index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
-import { worker } from "../api/msw/browser.ts";
+
 async function startMsw() {
   const { worker } = await import("../api/msw/browser.ts");
   return worker.start();
 }
-worker.start();
+
 startMsw().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
